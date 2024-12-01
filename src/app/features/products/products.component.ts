1b2e8e5b8cdd33ac32e2e7c3914f50f84c1fe74c
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-12-01 09:21:16 +0100

    Migrando toSignal

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,8 +1,9 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CartStateService } from 'src/app/store/cart-state/cart-state.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
+import { CartStateService } from 'src/app/store/cart-state/cart-state.service';
 import { CardComponent } from '@features/products/card/card.component';
 import { Product } from '@features/products/product.interface';
 import { ProductsService } from '@features/products/products.service';
@@ -19,7 +20,8 @@ export default class ProductsComponent implements OnInit {
   private readonly _productsService = inject(ProductsService);
   private readonly _cartService = inject(CartStateService);
 
-  products$ = this._productsService.products$;
+  // products$ = this._productsService.products$;
+  products$ = toSignal(this._productsService.products$);
 
   ngOnInit() {
     this._route.queryParams.subscribe((params) => {
