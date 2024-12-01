7a110ced3a9838a751be8cb649da9e3c5e862f0e
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-12-01 12:44:38 +0100

    Migrando Spinner Service

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 export class SpinnerService {
-  private isLoadingSubject = new BehaviorSubject<boolean>(false);
-  isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
+  // private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  private readonly isLoading = signal<boolean>(false);
+  // isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
+  isLoading$ = toObservable(this.isLoading);
 
   show(): void {
-    this.isLoadingSubject.next(true);
+    this.isLoading.set(true);
   }
 
   hide(): void {
-    this.isLoadingSubject.next(false);
+    this.isLoading.set(false);
   }
 }
