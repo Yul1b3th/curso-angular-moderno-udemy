b841c13e0c29f194cc3f2ef1860d5089edf087c3
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-12-01 10:38:02 +0100

    conversión inversa de Signal a Observable

--- src/app/store/cart-state/cart-state.service.ts
+++ src/app/store/cart-state/cart-state.service.ts
@@ -1,7 +1,7 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { Product } from '@features/products/product.interface';
 import { ToastrService } from 'ngx-toastr';
-import { BehaviorSubject } from 'rxjs';
 import { CartCalculatorService } from 'src/app/store/cart-state/cart-calculator.service';
 
 export interface CartStore {
@@ -18,20 +18,18 @@ export const initialCartState: CartStore = {
 
 @Injectable({ providedIn: 'root' })
 export class CartStateService {
-  private readonly _cartState = new BehaviorSubject<CartStore>(
-    initialCartState
-  );
   private readonly _cartCalculatorService = inject(CartCalculatorService);
   private readonly _toastrService = inject(ToastrService);
 
   private readonly _products = signal<Product[]>([]);
+
   readonly totalAmount = computed(() =>
     this._cartCalculatorService.calculateTotal(this._products())
   );
-  readonly productsCount = computed(() =>
-    this._cartCalculatorService.calculateTotal(this._products())
-  );
 
+  readonly productsCount = computed(() =>
+    this._cartCalculatorService.calculateItemsCount(this._products())
+  );
   // Global Store
   readonly cartStore = computed(() => ({
     products: this._products(),
@@ -39,15 +37,7 @@ export class CartStateService {
     totalAmount: this.totalAmount(),
   }));
 
-  cart$ = this._cartState.asObservable();
-
-  updateState(newState: CartStore): void {
-    this._cartState.next(newState);
-  }
-
-  getCurrentState(): CartStore {
-    return this._cartState.getValue();
-  }
+  cart$ = toObservable(this.cartStore);
 
   addToCart(product: Product): void {
     const currentProducts = this._products();
